2c8976544ecb27aa2866e8bfbd40401b9a6a047b
lengxia <1007705026@qq.com>
2017-08-25 12:31:11 +0800

    finall

--- xiayun.web/Controllers/HomeController.cs
+++ xiayun.web/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using PagedList;
 using System.Collections;
 using System.Text.RegularExpressions;
 using xiayun.web.Models;
+using System.IO;
 namespace xiayun.web.Controllers
 {
     [CustomAuth("", "")]
@@ -16,10 +17,11 @@ namespace xiayun.web.Controllers
         // GET: Home
         FileRepository fileinfo = new FileRepository();
         
-        public ActionResult Index(int? page,int? id,string filename,string nowdo,string path,string search)
+        public ActionResult Index(int? page,int? id,string filename,string nowdo,string path,string search,string attr)
         {
             int pag = page ?? 1;
             ViewData["page"] = pag;
+            ViewData["attr"] = attr;
             IPagedList<xiayun.Model.File> files;
             if (path == null)
             {
@@ -99,6 +101,21 @@ namespace xiayun.web.Controllers
                 ViewData["search"] = search;
                 return View(files);
             }
+            if (attr!=null){
+                string images = ".bmp,.jpg,.png,.tiff,.gif,.pcx,.tga,.exif,.fpx,.svg,.psd,.cdr,.pcd,.dxf,.ufo,.eps,.ai,.raw,.WMF";
+                string musics = ",.mp3,.wma,.wav,.asf,.aac,.vqf,.falc,.ape,.mid,.ogg";
+                string videos = ".rm,.rmvb,.mp4,.mov,.mtv,.dat,.wmv,.avi,.3gp,.amv,.dmv";
+                switch (attr)
+                {
+                    case "img": files = fileinfo.GetPaged(x => x.Belong ==Session["user"].ToString() & images.Contains(x.FileExt.ToLower()), x =>x.UpTime , pag, 10, false); break;
+                    case "music": files = fileinfo.GetPaged(x => x.Belong == Session["user"].ToString() & musics.Contains(x.FileExt.ToLower()), x => x.UpTime, pag, 10, false); break;
+                    case "video": files = fileinfo.GetPaged(x => x.Belong == Session["user"].ToString() & videos.Contains(x.FileExt.ToLower()), x => x.UpTime, pag, 10, false); break;
+                    default: files = fileinfo.GetPaged(x => x.Belong == Session["user"].ToString() , x => x.UpTime, pag, 10, false); break;
+
+
+                }
+                return View(files);
+            }
             files = fileinfo.GetPaged(x => x.Belong==Session["user"].ToString()&x.FilePath==path, x => x.UpTime, pag, 10, false);
             return View(files);
         }
@@ -123,6 +140,55 @@ namespace xiayun.web.Controllers
             }
             fileinfo.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public JsonResult PostedFile()
+        {
+
+            var file = HttpContext.Request.Files[0];
+            if (file == null)
+            {
+                return new JsonResult() { Data = "{\"code\":503,\"msg\":\"没有选择文件\"}" };
+            }
+            string fileName = Path.GetFileName(file.FileName);
+            string fileExt = Path.GetExtension(file.FileName);
+            string fileNameWithoutExt = Path.GetFileNameWithoutExtension(file.FileName);
+            string userName = Session["user"].ToString();
+            string fileRelativePath =Request.Form["path"];
+            try
+            {
+                string fileSaveDir = Server.MapPath("~\\upload\\" + Session["user"] +"\\"+ fileRelativePath);
+                if (!Directory.Exists(fileSaveDir))
+                {
+                    Directory.CreateDirectory(fileSaveDir);
+                }
+                string fileSavePath = fileSaveDir +"\\"+ fileName;
+                if (!System.IO.File.Exists(fileSavePath))
+                {
+                    file.SaveAs(fileSavePath);
+                }
+
+                var savefile = new xiayun.Model.File
+                {
+                    FileName = fileName,
+                    FileExt = fileExt,
+                    FilePath = fileRelativePath,
+                    Belong = Session["user"].ToString(),
+                     UpTime=DateTime.Now.ToString(),
+                };
+
+                fileinfo.Add(savefile);
+                fileinfo.SaveChanges();
+                return new JsonResult() { Data = "{\"code\":200,\"msg\":\"上传成功\"}" };
+
+            }
+            catch (Exception e)
+            {
+                return new JsonResult() { Data = "{\"code\":500,\"msg\":\"上传失败\"}" };
+
+            }
+
         }
     }
 }
